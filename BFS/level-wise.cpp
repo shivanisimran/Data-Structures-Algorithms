e2b2a1d2493b8e251a6f46a952fd19813670c447
shivanisimran <65017834+shivanisimran@users.noreply.github.com>
2021-05-31 23:00:04 +0530

    Update level-wise.cpp

--- BFS/level-wise.cpp
+++ BFS/level-wise.cpp
@@ -1,7 +1,7 @@
 /*when we need data for each level separately in a binary tree we collect the data separately for each level using a queue, and traversing the queue for all elements in a particular level.
 Then, similarly do for each level. */
 
-//Questions:
+//Here are some questions which use similar concepts:
 
 /*
 1. Binary Tree Level Order Traversal
