63bb11d2559ae64895894d8a1d1808b77767a6d6
shivanisimran <65017834+shivanisimran@users.noreply.github.com>
2021-07-24 19:51:20 +0530

    Update 0-1 Knapsack.cpp

--- Dynamic Programming/0-1 Knapsack type problems/0-1 Knapsack.cpp
+++ Dynamic Programming/0-1 Knapsack type problems/0-1 Knapsack.cpp
@@ -29,6 +29,8 @@ Output: 3 */
     }
 };
 
+
+
 //2.This method uses Memoization Technique (an extension of recursive approach).
 class Solution
 {
@@ -64,4 +66,31 @@ class Solution
        
     }
 };
+
+
+//3. Using Bottom-up(tabulation) approach:
+
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int w, int wt[], int val[], int n) 
+    { 
+       // Your code here
+       vector<vector<int>> dp(n+1, vector<int>(w+1));
+      
+       for(int i=0;i<=n;i++){
+           for(int j=0;j<=w;j++){
+               if(i==0 || j==0)
+                  dp[i][j]=0;
+               else if(wt[i-1]<=j)
+                   dp[i][j]=max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+               else
+                    dp[i][j]=dp[i-1][j];
+           }
+       }
+       return dp[n][w];
+    }
+};
+
 
