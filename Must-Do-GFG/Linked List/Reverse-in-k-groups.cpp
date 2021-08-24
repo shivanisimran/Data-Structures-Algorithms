0f80b329627d9d59943bfdf793602489c098dbeb
shivanisimran <65017834+shivanisimran@users.noreply.github.com>
2021-08-24 14:04:59 +0530

    Update Reverse-in-k-groups.cpp

--- Must-Do-GFG/Linked List/Reverse-in-k-groups.cpp
+++ Must-Do-GFG/Linked List/Reverse-in-k-groups.cpp
@@ -46,7 +46,97 @@ struct node *reverse (struct node *head, int k)
                 
         }
       
-        return prev;
-        
-        
+        return prev;     
     }
+
+
+/*One variation of above problem is when we don't have to reverse the leftover nodes.i.e, leave the node as it is when the size of remaining list is less than K.
+In that, case, we check the lenght of remaining list, weather it is >= k or not.
+
+Input: head = [1,2,3,4,5], k = 3
+Output: [3,2,1,4,5]
+
+Solution:                                                                                            */
+
+int length(ListNode* head){
+        int c=0;
+        while(head!=NULL){
+             c++;
+            head=head->next;
+        }
+        return c;   
+    }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+         ListNode* prev=NULL;
+         ListNode* p=prev;
+         while(head!=NULL){
+            int c=k;
+            ListNode* curr=new ListNode(-1);
+            if(length(head)>=c){
+                while(c--){
+                    int x=head->val;
+                    head=head->next;
+                    ListNode* z= new ListNode(x);
+                    if(curr->val>=0)
+                        z->next=curr;
+                    curr=z;
+                }
+            }
+            else{
+               curr=head;
+                head=NULL;
+            }
+            if(prev==NULL){
+                prev=curr;
+                p=prev;
+            }
+            else{
+                while(p->next){
+                    p=p->next;
+                }
+                p->next=curr;
+            }
+        }
+        return prev; 
+    }
+
+
+/*One more variation of this problem is to swap nodes pairwise. It can be solved using similar method, just putting K=2.
+Input:
+N = 7
+value[] = {1,2,3,4,5,6,7}
+Output: 2 1 4 3 6 5 7
+Explanation:The linked list after swapping
+becomes: 1 2 3 4 5 6 7 -> 2 1 4 3 6 5 7.
+
+Solution:                                                                                           */
+struct Node* pairwise_swap(struct Node* head)
+{
+    // your code here
+    Node* prev =NULL;
+    Node* p=prev;
+    while(head!=NULL){
+        Node* curr=new Node(-1);
+        int c=2;
+        while(c-- && head!=NULL){
+            int x=head->data;
+            head=head->next;
+            Node* z=new Node(x);
+            if(curr->data>=0){
+                z->next=curr;
+            }
+            curr=z;
+        }
+        if(prev==NULL){
+            prev=curr;
+            p=prev;
+        }
+        else{
+            while(p->next){
+                p=p->next;
+            }
+            p->next=curr;
+        }
+            
+    }
+    return prev;
