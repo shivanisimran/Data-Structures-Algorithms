9b215656943b97095b442a2b3e4168e95fe59805
shivanisimran <65017834+shivanisimran@users.noreply.github.com>
2021-09-09 13:59:48 +0530

    Create Max-Chunks-to-make-Sorted.cpp

---
+++ Monotonic Stack/Max-Chunks-to-make-Sorted.cpp
@@ -0,0 +1,27 @@
+/*
+You are given an integer array arr of length n that represents a permutation of the integers in the range [0, n - 1].
+We split arr into some number of chunks (i.e., partitions), and individually sort each chunk. After concatenating them, the result should equal the sorted array.
+Return the largest number of chunks we can make to sort the array.
+
+
+Example :
+Input: arr = [4,3,2,1,0]
+Output: 1
+Explanation:
+Splitting into two or more chunks will not return the required result.
+For example, splitting into [4, 3], [2, 1, 0] will result in [3, 4, 0, 1, 2], which isn't sorted.
+
+Solution: The basic idea is to use increasing Stack to keep track of the max value until the current position, and compare it to the sorted array. 
+          If the top of stack equals the element at index i in the sorted array, then the final count++.     
+CODE:                                                                                                              */
+int maxChunksToSorted(vector<int>& arr) {
+        stack<int> st;
+        int c=0;
+        for(int i=0;i<arr.size();i++){
+            if(st.empty() || arr[i]>st.top())
+                st.push(arr[i]);
+            if(st.top()==i)
+                c++;
+        }
+        return c;
+    }
