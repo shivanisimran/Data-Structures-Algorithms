58900c6db5773325896353ea62304659ece41875
shivanisimran <65017834+shivanisimran@users.noreply.github.com>
2021-09-09 15:50:49 +0530

    Update Max-Chunks-to-make-Sorted.cpp

--- Monotonic Stack/Max-Chunks-to-make-Sorted.cpp
+++ Monotonic Stack/Max-Chunks-to-make-Sorted.cpp
@@ -32,6 +32,13 @@ You are given an integer array arr.
 We split arr into some number of chunks (i.e., partitions), and individually sort each chunk. After concatenating them, the result should equal the sorted array.
 Return the largest number of chunks we can make to sort the array.
 
+Example :
+Input: arr = [2,1,3,4,4]
+Output: 4
+Explanation:
+We can split into two chunks, such as [2, 1], [3, 4, 4].
+However, splitting into [2, 1], [3], [4], [4] is the highest number of chunks possible.
+
 Solution:                                                                                                */
 int maxChunksToSorted(vector<int>& arr) {
         int n=arr.size();
