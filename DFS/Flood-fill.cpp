5400bd2d5bb6a4c5ea174c0e69e4969676f00616
shivanisimran <65017834+shivanisimran@users.noreply.github.com>
2021-10-04 20:37:13 +0530

    Create Flood-fill.cpp

---
+++ DFS/Flood-fill.cpp
@@ -0,0 +1,36 @@
+/*
+An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+You are also given three integers sr, sc, and newColor. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel 
+of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. 
+Replace the color of all of the aforementioned pixels with newColor.
+
+Return the modified image after performing the flood fill.
+
+Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, newColor = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the 
+same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
+
+Solution:                                                                                                                                     */
+   vector<vector<int>> floodFill(vector<vector<int>>& im, int r, int c, int ne) {
+        int x=im[r][c];
+        int n=im.size(),m=im[0].size();
+        vector<vector<int>>vis(n);
+        for(int i=0;i<n;i++)    vis[i]=vector<int>(m,0);
+        
+        dfs(im,vis,x,r,c,ne);
+        return im;
+    }
+    void dfs(vector<vector<int>>&im,vector<vector<int>>&vis,int x,int r,int c,int ne){
+        if((r>=0 && r<im.size()) && (c>=0 && c<im[0].size()) && im[r][c]==x && !vis[r][c]){
+            vis[r][c]=1;
+            im[r][c]=ne;
+            dfs(im,vis,x,r-1,c,ne);
+            dfs(im,vis,x,r+1,c,ne);
+            dfs(im,vis,x,r,c-1,ne);
+            dfs(im,vis,x,r,c+1,ne);
+        }
+        else  return;
+    }
