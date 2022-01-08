01a4201fccfb7f37a3eacf7fb4913a1b6585c69a
Shivani Simran <65017834+shivanisimran@users.noreply.github.com>
2022-01-08 17:38:23 +0530

    Update Count-subset-with-given-sum(Perfect Sum Problem).cpp

--- Dynamic Programming/2-D DP/0-1 Knapsack Types/Count-subset-with-given-sum(Perfect Sum Problem).cpp
+++ Dynamic Programming/2-D DP/0-1 Knapsack Types/Count-subset-with-given-sum(Perfect Sum Problem).cpp
@@ -12,24 +12,24 @@ Explanation: {2, 3, 5}, {2, 8}, {10}
 
 Solution:                                                                                                                                              */
 	int mod=1000000007;
-	int kmp(int arr[], int n, int s, vector<vector<int>>& dp ){
+	int knp(int arr[], int n, int s, vector<vector<int>>& dp ){
 	    if(s==0) return 1;
 	    if(n==0) return 0;
 	    
 	    if(dp[n][s]!=-1)  return dp[n][s];
 	    
-	    if(arr[n-1]<=s) return dp[n][s]=(kmp(arr,n-1,s-arr[n-1],dp)+kmp(arr,n-1,s,dp))%mod;
-	    else return dp[n][s]=kmp(arr,n-1,s,dp)%mod;
+	    if(arr[n-1]<=s) return dp[n][s]=(knp(arr,n-1,s-arr[n-1],dp)+knp(arr,n-1,s,dp))%mod;
+	    else return dp[n][s]=knp(arr,n-1,s,dp)%mod;
 	}
 
 	int perfectSum(int arr[], int n, int sum)
 	{
-        // Your code goes here
-       vector<vector<int>>dp(n+1);
-       int z=0;
-       for(int i=0;i<n;i++) if(arr[i]==0) z++;
-       for(int i=0;i<=n;i++) dp[i]=vector<int>(sum+1,-1);
-       
-       return (kmp(arr,n,sum,dp)+(1 << z)-1)%mod;
+               // Your code goes here
+	       vector<vector<int>>dp(n+1);
+	       int z=0;
+	       for(int i=0;i<n;i++) if(arr[i]==0) z++;
+	       for(int i=0;i<=n;i++) dp[i]=vector<int>(sum+1,-1);
+
+	       return (knp(arr,n,sum,dp)+(1 << z)-1)%mod;
 	}
 
