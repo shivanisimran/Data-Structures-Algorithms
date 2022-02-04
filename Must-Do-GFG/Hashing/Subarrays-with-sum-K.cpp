710031674617d83ba44ec1f011f7fd39c3438014
Shivani Simran <65017834+shivanisimran@users.noreply.github.com>
2022-02-04 16:43:09 +0530

    Update Subarrays-with-sum-K.cpp

--- Must-Do-GFG/Hashing/Subarrays-with-sum-K.cpp
+++ Must-Do-GFG/Hashing/Subarrays-with-sum-K.cpp
@@ -24,3 +24,68 @@ Solution:                                                                       
         }
         return ans;
     }
+
+
+# **Similar Problems:**
+
+/* 
+Longest Sub-Array with Sum K:
+
+Given an array containing N integers and an integer K., 
+Your task is to find the length of the longest Sub-Array with the sum of the elements equal to the given value K.
+
+Input :
+A[] = {10, 5, 2, 7, 1, 9}
+K = 15
+Output : 4
+Explanation:
+The sub-array is {5, 2, 7, 1}.
+
+Solution:                                                                                                                            */
+    int lenOfLongSubarr(int a[],  int n, int k) 
+    { 
+        // Complete the function
+        unordered_map<int,int> mp;
+        mp[0]=0;
+        int s=0,ans=0;
+        for(int i=0;i<n;i++){
+            s+=a[i];
+            if(s==k)  ans=i+1;
+            
+            if(!mp[s]) mp[s]=i+1;
+            
+            if(mp[s-k]>0) ans=max(ans,i+1-mp[s-k]);
+        }
+        return ans;
+    } 
+
+/*
+
+Contiguous Array:
+
+Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
+
+Input: nums = [0,1]
+Output: 2
+Explanation: [0, 1] is the longest contiguous subarray with an equal number of 0 and 1.
+
+Solution:                                                                                                                                            */
+    int findMaxLength(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        mp[0]=0;
+        int z=0,o=0,n=nums.size(),ans=0;
+        
+        for(int i=0;i<n;i++){
+            if(nums[i]==0)  z++;
+            else    o++;
+            
+            if(!mp[z-o]) mp[z-o]=i+1;
+            if(z-o==0)  ans=i+1;
+            
+           // cout<<(z-o)<<" "<<mp[z-o]<<endl;
+            ans=max(ans,i+1-(mp[z-o]));
+   
+        }
+        
+        return ans;
+    }
